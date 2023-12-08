5ca5a755c6e7632ba0a42a68ab2e2fd85527b418
naturalemblem <naturalemblem@gmail.com>
2023-12-07 23:42:35 -0500

    updates to hangman and upload simpletodolist

--- Hangman/app/src/main/java/com/example/kpizani/hangman/MainActivity.java
+++ Hangman/app/src/main/java/com/example/kpizani/hangman/MainActivity.java
@@ -1,21 +1,17 @@
 package com.example.kpizani.hangman;
 
-import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridLayout;
-import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-
-import static java.lang.System.in;
 
 public class MainActivity extends AppCompatActivity {
     GridLayout gridL;
